831ca4c57b1a50bcab45da2db4a317a4daee1d02
TheMiguelGt <miguel.74ngel@gmail.com>
2023-03-03 15:55:07 -0600

    signup view and configure in urls

--- adopt/adopt/urls.py
+++ adopt/adopt/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('',include('core.urls')),
     # Auth paths
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.urls')),
 ]
